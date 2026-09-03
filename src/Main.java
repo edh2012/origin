f4878aef9b03166160efa17bf35644b53bd28876
이두현 <edh2012@naver.com>
2026-09-03 11:30:36 +0900

    2주차 추가

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,2 @@
-import java.util.Scanner;
-
-void main() {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("나이: ");
-    int age = scanner.nextInt();
-    System.out.print("이름: ");
-    String name = scanner.nextLine();
-// 남아 있던 줄바꿈을 읽어 name이 빈 문자열
-    System.out.println(name);
+public class Main {
 }
